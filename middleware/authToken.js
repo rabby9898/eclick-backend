a7f86db10b765f0a216788008875e61242329af1
rabby9898 <mdfajlerabby00@gmail.com>
2024-05-10 11:57:35 +0600

    token error handling added

--- middleware/authToken.js
+++ middleware/authToken.js
@@ -1,10 +1,31 @@
+const jwt = require("jsonwebtoken");
+
 const authToken = async (req, res, next) => {
   try {
-    const token = req.cookies || req.header;
+    const token = req.cookies?.token;
     console.log("token", token);
+
+    if (!token) {
+      return res.json({
+        message: "User not logged in",
+        error: true,
+        success: false,
+      });
+    }
+    jwt.verify(token, process.env.TOKEN_SECRET_KEY, function (err, decoded) {
+      console.log("jwt-errror------", err);
+      console.log("Decoded user", decoded);
+    });
+
+    if (err) {
+      console.log("Error Auth", err);
+    }
+    req.user.id = decoded._id;
+    next();
   } catch (err) {
     res.status(400).json({
       message: err.message || err,
+      data: [],
       error: true,
       success: false,
     });
--- routes/routes.js
+++ routes/routes.js
@@ -2,11 +2,12 @@ const express = require("express");
 const signUpController = require("../controller/signup");
 const useSignInController = require("../controller/signin");
 const userDetailsCOntroller = require("../controller/userDetailsCOntroller");
+const authToken = require("../middleware/authToken");
 
 const router = express.Router();
 
 router.post("/signup", signUpController);
 router.post("/signin", useSignInController);
-router.get("/user-details", userDetailsCOntroller);
+router.get("/user-details", authToken, userDetailsCOntroller);
 
 module.exports = router;

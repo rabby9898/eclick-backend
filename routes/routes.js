5fd277f8d6e8b493d6bacd9dd5a57644d0647330
rabby9898 <mdfajlerabby00@gmail.com>
2024-05-11 07:11:34 +0600

    logout user api added

--- routes/routes.js
+++ routes/routes.js
@@ -3,11 +3,13 @@ const signUpController = require("../controller/signup");
 const useSignInController = require("../controller/signin");
 const userDetailsCOntroller = require("../controller/userDetailsCOntroller");
 const authToken = require("../middleware/authToken");
+const userLogout = require("../controller/userLogout");
 
 const router = express.Router();
 
 router.post("/signup", signUpController);
 router.post("/signin", useSignInController);
 router.get("/user-details", authToken, userDetailsCOntroller);
+router.get("/logout", authToken, userLogout);
 
 module.exports = router;

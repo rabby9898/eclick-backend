2f3c848f60c7ef79a2e136bacb7b6e1c8d29ad71
rabby9898 <mdfajlerabby00@gmail.com>
2024-06-09 14:15:53 +0600

    DEPLOYMENT RENDER

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 var cookieParser = require("cookie-parser");
 const connectDB = require("./config/db");
 const router = require("./routes/routes");
@@ -8,18 +8,26 @@ require("dotenv").config();
 const app = express();
 
 // Configure CORS
-// const corsOptions = {
-//   origin: [
-//     "http://localhost:5174",
-//     "http://localhost:5173",
-//     "http://localhost:5000",
-//     // "https://eclick-ecommerce.web.app",
-//   ],
-//   // credentials: true,
-// };
+// Configure CORS
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:5174",
+  "http://localhost:5000",
+  "https://eclick-ecommerce.web.app",
+];
 
-// app.use(cors(corsOptions));
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
 
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api", router);
